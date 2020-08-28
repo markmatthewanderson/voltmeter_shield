0a654927ead7f872742079be6c298ec7c694cf29
aweid10 <37081040+aweid10@users.noreply.github.com>
2020-08-27 19:58:40 -0500

    Psuedo Code

--- fw/voltmeter/voltmeter.ino
+++ fw/voltmeter/voltmeter.ino
@@ -1,9 +1,71 @@
-void setup() {
-  // put your setup code here, to run once:
+
+//Voltmeter Code
+
+//Mark Anderson
+//Nick Robillard
+//Adam Weidling
+
+//Decode input voltage and autoscale voltage range. Output voltage to 7-segment display
+
+//Pin Definitions
+
+int ADC_Scale = 5;    //Voltage Scale for ADC
+int VIn_Pin = 3;      //Pin Reading Analog Voltage
+
+int 5V_Led_Pin = 6;   //LED for 0-5V Case
+int 15V_Led_Pin = 7;  //LED for 5-15V Case
+int 30V_Led_Pin = 8;  //LED for 15-30V Case
+
+
+void setup() 
+{
+  
+Serial.begin(9600);              //Serial Monitor for Debugging
+
+//Set Pins to Input/Output
+
+
+
+
 
 }
 
-void loop() {
+
+
+void loop() 
+{
   // put your main code here, to run repeatedly:
+
+
+
+Voltage = analogRead(VIn_Pin)*(ADC_Scale/1023);	                  %Read voltage in 
+
+if (Voltage < 5)
+{
+
+
+//Check to see if you need to bump up a scale or down a scale
+  
+}
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+void sevsegdisp()
+{
+
+%Decode voltage and output on display
 
 }
