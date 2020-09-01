5a3da21473c48cbe824e108c635fb9e99f3052e8
aweid10 <37081040+aweid10@users.noreply.github.com>
2020-09-01 15:28:10 -0500

    Update voltmeter.ino
    Updated Psuedo Code

--- fw/voltmeter/voltmeter.ino
+++ fw/voltmeter/voltmeter.ino
@@ -11,14 +11,29 @@
 
 //Pin Definitions
 
+///////////////////////////////////////////////////////////////
+int ADC_Scale = 4.096;    //Voltage Scale for ADC
+int VIn_Pin_1 = 1;          //Pin Reading Analog Voltage
+int VIn_Pin_2
 
-int ADC_Scale = 5;    //Voltage Scale for ADC
-int VIn_Pin = 3;      //Pin Reading Analog Voltage
+///////////////////////////////////////////////////////////////
 
-int 5V_Led_Pin = 6;   //LED for 0-5V Case
-int 15V_Led_Pin = 7;  //LED for 5-15V Case
-int 30V_Led_Pin = 8;  //LED for 15-30V Case
+//Switch Pins for Scaling
+int 1_2_Led_Switch = 5;  //LED for 0-1.2V Case
+int 5V_Led_Switch = 6;   //LED for 1.2-5V Case
+int 15V_Led_Switch = 7;  //LED for 5-15V  Case 
+int 30V_Led_Switch = 8;  //LED for 15-30V Case
+//////////////////////////////////////////////////////////////
 
+//HC595 Pin Setup
+int dataPin = 8;
+int latchPin = 9;
+int clockPin = 10;
+//////////////////////////////////////////////////////////////
+
+//7-Segment Digit Pins
+int pins [] = {1,2,3,4};
+//////////////////////////////////////////////////////////////
 
 void setup() 
 {
@@ -27,12 +42,28 @@ Serial.begin(9600);              //Serial Monitor for Debugging
 
 //Set Pins to Input/Output
 
-pinMode(VIn_Pin, INPUT);
+//Shift Register Pins
+pinMode(dataPin,OUTPUT);
+pinMode(latchPin,OUTPUT);
+pinMode(clockPin,OUTPUT);
+
+//Digit Pins as Output
+  for(i=0; i<sizeof(pins);i++)
+  {
+    pinMode(pins[i],OUTPUT);
+  }
+
+
+
+//Analog Read Pin Input
+pinMode(VIn_Pin_1, INPUT);
+pinMode(Vin_Pin_2, INPUT);
 
 //LED Output Pins
-pinMode(5V_Led_Pin, OUTPUT);
-pinMode(15V_Led_Pin, OUTPUT);
-pinMode(30V_Led_Pin, OUTPUT);
+pinMode(1_2_Led_Switch, OUTPUT);
+pinMode(5V_Led_Switch,  OUTPUT);
+pinMode(15V_Led_Switch, OUTPUT);
+pinMode(30V_Led_Switch, OUTPUT);
 
 
 
@@ -47,9 +78,20 @@ void loop()
 
 
 
-Voltage = analogRead(VIn_Pin)*(ADC_Scale/1023);	                  %Read voltage in 
+//Check to see if the voltage on the first ADC is in the 0-1.2V Scale
+Voltage_1 = analogRead(VIn_Pin_1)*(ADC_Scale/1023);	      //Read ADC and convert to Voltage
 
-if (Voltage < 5)
+
+//Call 7-Seg function and outptut
+
+
+
+//Read in voltage on second ADC
+Voltage_2 = analogRead(VIn_Pin_2)*(ADC_Scale/1023);
+
+
+//Check to see if voltage is in the 0-5V Scale
+if (Voltage < 4.096)
 {
 
 
