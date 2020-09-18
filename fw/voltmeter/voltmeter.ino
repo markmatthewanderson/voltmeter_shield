581991c0dbcbe5bb31520227aebe6ce7e3876cc4
Mark Matthew Anderson <markmatthewanderson@gmail.com>
2020-09-17 20:33:57 -0500

    Add pin mapping to Arduino code.
    Added pin mapping to Arduino code with #define statements.

--- fw/voltmeter/voltmeter.ino
+++ fw/voltmeter/voltmeter.ino
@@ -10,13 +10,14 @@
 //Decode input voltage and autoscale voltage range. Output voltage to 7-segment display
 
 //Pin Definitions
-
 ///////////////////////////////////////////////////////////////
 int ADC_Scale = 4.096;    //Voltage Scale for ADC
 int VIn_Pin_1 = 1;          //Pin Reading Analog Voltage
 int VIn_Pin_2
 int Buffer = 20;         //20% Bufffer
 
+#define ADC_UNITY_PIN 9
+#define ADC_GAIN_PIN  10
 ///////////////////////////////////////////////////////////////
 
 //Switch Pins for Scaling
@@ -24,17 +25,37 @@ int 1_2_Led_Switch = 5;  //LED for 0-1.2V Case
 int 5V_Led_Switch = 6;   //LED for 1.2-5V Case
 int 15V_Led_Switch = 7;  //LED for 5-15V  Case 
 int 30V_Led_Switch = 8;  //LED for 15-30V Case
+#define SCALE_1V_PIN    15
+#define SCALE_5V_PIN    16
+#define SCALE_15V_PIN   17
+#define SCALE_30V_PIN   18
 //////////////////////////////////////////////////////////////
 
 //HC595 Pin Setup
 int dataPin = 8;
 int latchPin = 9;
 int clockPin = 10;
+#define SR_RCLK_PIN   25
+#define SR_SER_PIN    26
+#define SR_SRCLK_PIN  28
 //////////////////////////////////////////////////////////////
 
 //7-Segment Digit Pins
-int pins [] = {1,2,3,4};
+#define SS_DIGIT1_PIN 19
+#define SS_DIGIT2_PIN 20
+#define SS_DIGIT3_PIN 21
+#define SS_DIGIT4_PIN 22
+int pins [] = {SS_DIGIT1_PIN, SS_DIGIT2_PIN, SS_DIGIT3_PIN, SS_DIGIT4_PIN};
 //////////////////////////////////////////////////////////////
+
+//Type for tracking current scale state
+enum state 
+{
+  SCALE_1V,
+  SCALE_5V,
+  SCALE_15V,
+  SCALE_30V
+};
 
 void setup() 
 {
